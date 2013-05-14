011cee27995853eff9007bfa92aede964fb18799
jepeloqu <jepeloqu@gmail.com>
2013-05-14 10:54:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LesYourtesDeLaRiviere::Application.config.secret_key_base = '562208dbd013d48ac65e6026e71cbb5312b10c81467cf3c4d5ab800576aa9f17e7c67da3d29a48855e8b3389bc97473718a3d2cfe45ea0484a00e05e97aeca91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LesYourtesDeLaRiviere::Application.config.session_store :encrypted_cookie_store, key: '_les_yourtes_de_la_riviere_session'
